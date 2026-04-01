253880b085086810f9418f6462db41b2878886d0
Alex Edmondo <97455272+edmondoalex@users.noreply.github.com>
2026-04-01 12:22:07 +0200

    Allow editing connection settings in options

--- custom_components/dahua_event_listener/config_flow.py
+++ custom_components/dahua_event_listener/config_flow.py
@@ -45,12 +45,32 @@ class DahuaOptionsFlowHandler(config_entries.OptionsFlow):
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
+            new_data = {
+                **self.config_entry.data,
+                "host": user_input["host"],
+                "username": user_input["username"],
+                "password": user_input["password"],
+                "channels": user_input["channels"],
+            }
+            self.hass.config_entries.async_update_entry(self.config_entry, data=new_data)
+
+            options = {
+                "connect_timeout": user_input["connect_timeout"],
+                "read_timeout": user_input["read_timeout"],
+                "idle_reconnect_seconds": user_input["idle_reconnect_seconds"],
+                "reconnect_delay": user_input["reconnect_delay"],
+            }
+            return self.async_create_entry(title="", data=options)
 
         options = self.config_entry.options
+        data = self.config_entry.data
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
+                vol.Required("host", default=data.get("host", "")): str,
+                vol.Required("username", default=data.get("username", "")): str,
+                vol.Required("password", default=data.get("password", "")): str,
+                vol.Required("channels", default=data.get("channels", 1)): vol.All(vol.Coerce(int), vol.Range(min=1, max=256)),
                 vol.Optional("connect_timeout", default=options.get("connect_timeout", 10)): vol.All(vol.Coerce(int), vol.Range(min=5, max=120)),
                 vol.Optional("read_timeout", default=options.get("read_timeout", 60)): vol.All(vol.Coerce(int), vol.Range(min=5, max=300)),
                 vol.Optional("idle_reconnect_seconds", default=options.get("idle_reconnect_seconds", 120)): vol.All(vol.Coerce(int), vol.Range(min=30, max=600)),
